ab40babd2ab1bcbc9d2373b1f7446308e68027d1
Giedre <giedre.klima@gmail.com>
2021-12-22 22:45:16 +0200

    interactions in component trees

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { FaStar } from "react-icons/fa";
+
+const createArray = (length) => [...Array(length)];
+
+function Star({ selected = false, onSelect }) {
+  return <FaStar color={selected ? "gold" : "gray"} onClick={onSelect} />;
+}
+
+function StarRating({ totalStars = 5 }) {
+  const [selectedStars, setSelectedStars] = useState(0);
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>
+        {selectedStars} of {totalStars}
+      </p>
+    </>
+  );
+}
 
 function App() {
-  const [checked, setChecked] = useState(false);
-
-  return (
-    <div>
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={() => setChecked((checked) => !checked)}
-      />
-      <p>{checked ? "checked" : "not checked"}</p>
-    </div>
-  );
+  return <StarRating totalStars={7} />;
 }
 
 ReactDOM.render(
