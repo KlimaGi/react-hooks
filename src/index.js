68ad7f4c5e5acfb3d694f2ffb57d294a383ceb18
Giedre <giedre.klima@gmail.com>
2022-01-14 13:44:02 +0200

    fetching data with Fech Hook, do a fech component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
-import React, { createContext, useContext } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { App } from "./App";
+import { useFetch } from "./useFetch";
 
-const TreesContext = createContext();
+const App = ({ login }) => {
+  const { loading, data, error } = useFetch(
+    `https://api.github.com/users/${login}`
+  );
+  if (loading) return <h3>loading...</h3>;
+  if (error) {
+    return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  }
+  return (
+    <div>
+      <img src={data.avatar_url} alt={data.login} />
+      <div>
+        <h3>{data.login}</h3>
+        {data.name && <p>{data.name}</p>}
+        {data.location && <p>{data.location}</p>}
+      </div>
+    </div>
+  );
+};
 
-export const useTrees = () => useContext(TreesContext);
-
-const trees = [
-  { id: "1", type: "Maple" },
-  { id: "2", type: "Oak" },
-  { id: "3", type: "Pine" },
-  { id: "4", type: "Birch" },
-];
-
-ReactDOM.render(
-  <TreesContext.Provider value={{ trees }}>
-    <App />
-  </TreesContext.Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App login="KlimaGi" />, document.getElementById("root"));
